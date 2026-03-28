9d600713cf6da70c840b8045d851303dec7168dd
stngmz1-wq <stngmz1@gmail.com>
2026-03-28 10:03:17 -0500

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-03-2026 a las 02:27:22
+-- Tiempo de generación: 28-03-2026 a las 16:02:16
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
